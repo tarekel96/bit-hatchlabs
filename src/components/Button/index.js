d9c227ff45b1ac8139c14782233ad9c23d7f14f3
Tarek El-Hajjaoui <tarekel96@gmail.com>
2020-05-08 16:32:20 -0700

    committing repository to git

---
+++ src/components/Button/index.js
@@ -0,0 +1,73 @@
+// import packages
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+// import icon components from react-icons npm
+import { FiArrowLeft } from "react-icons/fi";
+import { FiArrowRight } from "react-icons/fi";
+
+// import style module
+import styles from "./button.module.scss";
+
+/**
+ * A simple React card component
+ * @name Button
+ * @returns {} returns a simple button component with various prop options to change it's styling
+ */
+export default function Button(props) {
+  const [clicked, setClick] = useState("");
+  return (
+    <button
+      className={`
+        button 
+        ${styles[props.version + clicked]}
+        ${styles[props.size]}
+  `}
+      onClick={() => {
+        setClick("-clicked");
+      }}
+      size={props.size}
+    >
+      <p>
+        <span className={`${styles["icon-left"]}`}>
+          {props.leftIcon ? <FiArrowLeft /> : ""}
+        </span>
+        <span className="button-content">{props.content}</span>
+        <span className={`${styles["icon-right"]}`}>
+          {props.rightIcon ? <FiArrowRight /> : ""}
+        </span>
+      </p>
+    </button>
+  );
+}
+
+Button.propTypes = {
+  /**
+   * The content for the button
+   */
+  content: PropTypes.string,
+  /**
+   * The version of a button ie primary, secondary, text, etc
+   */
+  version: PropTypes.string,
+  /**
+   * The size for a button: "big" or "small"
+   */
+  size: PropTypes.string,
+  /**
+   * Includes an icon to the left of the content
+   */
+  leftIcon: PropTypes.bool,
+  /**
+   * Includes an icon to the right of the content
+   */
+  rightIcon: PropTypes.bool,
+  /**
+   * Includes a squigglyLine if provided
+   */
+  squigglyLine: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  content: "Content",
+};
---
+++ src/pages/buttons-dark-mode.js
@@ -0,0 +1,111 @@
+import React from "react";
+
+import Button from "../components/Button/index";
+
+export default function buttonsLightMode() {
+  return (
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        textAlign: "center",
+        marginTop: "3rem",
+      }}
+    >
+      <div>
+        <h3>Primary</h3>
+        <Button content="Primary" version="primary" size="small" />
+        <Button content="primary big" version="primary" size="big" />
+        <Button content="Clicked" version="primary-clicked" />
+        <Button content="Disabled" version="primary-disabled" />
+        <Button content="Danger" version="danger" />
+        <Button content="Danger Big" version="danger" size="big" />
+      </div>
+      <div>
+        <h3>Secondary</h3>
+        <Button version="secondary" size="small" />
+        <Button content="secondary big" version="secondary" size="big" />
+        <Button content="Clicked" version="secondary-clicked" />
+        <Button content="Disabled" version="secondary-disabled" />
+      </div>
+      <div>
+        <h3>Text Default</h3>
+        <div>
+          <Button content="Text" version="text" size="small" />
+          <Button content="Text Big" version="text" size="big" />
+          <Button content="Clicked" version="text-clicked" size="small" />
+          <Button content="Disabled" version="text-disabled" size="small" />
+        </div>
+        <h3>Text Yellow</h3>
+        <div>
+          <Button content="Text" version="text-yellow" size="small" />
+          <Button content="Text Big" version="text-yellow" size="big" />
+          <Button
+            content="Clicked"
+            version="text-yellow-clicked"
+            size="small"
+          />
+          <Button content="Disabled" version="text-disabled" size="small" />
+        </div>
+        <h3>Text Orange</h3>
+        <div>
+          <Button content="Text" version="text-orange" size="small" />
+          <Button content="Text Big" version="text-orange" size="big" />
+          <Button
+            content="Clicked"
+            version="text-orange-clicked"
+            size="small"
+          />
+          <Button content="Disabled" version="text-disabled" size="small" />
+        </div>
+        <h3>Text Blue</h3>
+        <div>
+          <Button content="Text" version="text-blue" size="small" />
+          <Button content="Text Big" version="text-blue" size="big" />
+          <Button content="Clicked" version="text-blue-clicked" size="small" />
+          <Button content="Disabled" version="text-disabled" size="small" />
+        </div>
+        <h3>Text Aqua</h3>
+        <div>
+          <Button content="Text" version="text-aqua" size="small" />
+          <Button content="Text Big" version="text-aqua" size="big" />
+          <Button content="Clicked" version="text-aqua-clicked" size="small" />
+          <Button content="Disabled" version="text-disabled" size="small" />
+        </div>
+      </div>
+      <div>
+        <h3>Text with Icons</h3>
+        <Button content="Text Left Icon" leftIcon version="text" size="small" />
+        <Button
+          content="Clicked"
+          leftIcon
+          version="text-clicked"
+          size="small"
+        />
+        <Button content="Text Left Icon" leftIcon version="text" size="big" />
+        <Button content="Clicked" leftIcon version="text-clicked" size="big" />
+        <Button
+          content="Text Right Icon"
+          rightIcon
+          version="text"
+          size="small"
+        />
+        <Button
+          content="Clicked"
+          rightIcon
+          version="text-clicked"
+          size="small"
+        />
+        <Button content="Text Right Icon" rightIcon version="text" size="big" />
+        <Button content="Clicked" rightIcon version="text-clicked" size="big" />
+      </div>
+      <div>
+        <h3>Text with Link</h3>
+        <Button content="Text" version="text-link" size="small" />
+        <Button content="Link Big" version="text-link" size="big" />
+        <Button content="Clicked" version="text-link-clicked" size="small" />
+      </div>
+    </section>
+  );
+}
