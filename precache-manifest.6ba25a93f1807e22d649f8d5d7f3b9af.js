a12ec8a748aa14c957de599aa061fd914d5e9b27
Tarek El-Hajjaoui <tarekel96@gmail.com>
2020-05-13 12:00:29 -0700

    Updates

---
+++ precache-manifest.6ba25a93f1807e22d649f8d5d7f3b9af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3b0d71b32c0b52ee968eb6d8829d58",
+    "url": "/bit-hatchlabs/index.html"
+  },
+  {
+    "revision": "dd35c035366c618490cb",
+    "url": "/bit-hatchlabs/static/css/main.c1f0cf50.chunk.css"
+  },
+  {
+    "revision": "b3da2652677b2a08fefd",
+    "url": "/bit-hatchlabs/static/js/2.839c556c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bit-hatchlabs/static/js/2.839c556c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd35c035366c618490cb",
+    "url": "/bit-hatchlabs/static/js/main.67609cb9.chunk.js"
+  },
+  {
+    "revision": "01012cc51b067a2a37c2",
+    "url": "/bit-hatchlabs/static/js/runtime-main.cd5d7c9a.js"
+  },
+  {
+    "revision": "6df674bff55ef1b16adc552dedc5908c",
+    "url": "/bit-hatchlabs/static/media/bubble-primary.6df674bf.svg"
+  },
+  {
+    "revision": "fa6417269d5a28251e48ef39b6017a73",
+    "url": "/bit-hatchlabs/static/media/bubble-secondary.fa641726.svg"
+  },
+  {
+    "revision": "9334ca461eba0336a53226195ea02356",
+    "url": "/bit-hatchlabs/static/media/bubble-text-dmode.9334ca46.svg"
+  },
+  {
+    "revision": "97c47671547866bdfcf51f7e157da7f9",
+    "url": "/bit-hatchlabs/static/media/bubble-text.97c47671.svg"
+  },
+  {
+    "revision": "cbc6708fb51c5b97d6577da6f12b6fd9",
+    "url": "/bit-hatchlabs/static/media/bubble.cbc6708f.svg"
+  }
+]);
